db3d67f67f33d9329678e87f0c05028e145ca63a
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2025-10-10 16:25:51 +0300

    Add files via upload

---
+++ diyalogKutulari.js
@@ -0,0 +1,13 @@
+//alert uyarı mesajı verir.
+alert("Merhaba Çisem");
+
+//prompt kullanıcıdan bilgi alıyor
+let isim=prompt("İsminizi Giriniz");
+console.log("isminiz: "+isim);
+let soyisim=prompt("Soyisminizi Giriniz");
+console.log("Soyisminiz: "+soyisim);
+
+//confirm kutucuğu
+let sonuc=confirm("Silmek istediğinize emin misiniz?");
+
+Swal.fire("merhaba Çisem");
