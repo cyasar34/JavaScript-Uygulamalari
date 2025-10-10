4e67f329e761b4496d8013f05321afdd1698e125
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2025-10-10 16:27:34 +0300

    Add files via upload

---
+++ ConsoleCiktiVermeMesajlari3.js
@@ -0,0 +1,30 @@
+console.log("Çisem"); //string
+console.log(15); //integer
+console.log(3.14); //float ya da double
+console.log(true); //boolean
+
+
+//window objesinin tüm özelliklerini görme
+console.dir(window);
+
+//mevcut sayfanın alan adını öğrenme
+console.dir(window.document.location.host);
+
+
+//documentURL yazdırma
+console.dir(window.document.documentURI);
+
+//port numarasını öğrenme
+console.dir(window.location.port);
+
+
+
+
+
+//writeln kullanımı
+document.writeln("Çisem Yaşar" + "<br>"); //string ifade
+document.writeln(18 + "<br>"); //integer ifade
+document.writeln(3.14 + "<br>"); //float yada double ifade
+document.writeln(true + "<br>"); //boolean ifade
+document.writeln(null + "<br>"); //obje ifade
+
